2a4851755b30ed5769ace4381421a35a7caaaa4d
Candance Smith <Candance@candances-macbook-air.local>
2016-05-16 15:21:48 -0400

    Initial Commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Candance Smith on 5/16/16.
+//  Copyright © 2016 candance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Matchismo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Matchismo
+//
+//  Created by Candance Smith on 5/16/16.
+//  Copyright © 2016 candance. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
